0b25b3984b6f59a7c8f3682ee2b50b5fd9e84bee
catsymptote <catsymptote@gmail.com>
2020-02-04 15:06:19 +0100

    Solved and tested task 4 in set 1 diff 1.

---
+++ app/set1/difficulty1/task4.py
@@ -0,0 +1,61 @@
+def single_match(balance, weights):
+    for weight in weights:
+        if balance[0] == weight + balance[1]:
+            return weight
+        elif balance[1] == weight + balance[0]:
+            return weight
+    return None
+
+
+def double_balance(balance, w1, w2):
+    if balance[0]           == balance[1] + w1 + w2:
+        return True
+    elif balance[1]         == balance[0] + w1 + w2:
+        return True
+    elif balance[0] + w1    == balance[1] + w2:
+        return True
+    elif balance[0] + w2    == balance[1] + w1:
+        return True
+    else:
+        return False
+
+
+def double_match(balance, weights):
+    for i1 in range(len(weights)):
+        remaining = weights.copy()
+        w1 = remaining.pop(i1)
+        for i2 in range(len(remaining)):
+            w2 = remaining[i2]
+            if double_balance(balance, w1, w2):
+                solution = [w1, w2]
+                solution.sort()
+                return solution
+    return None
+
+
+def balancing(balance:list, weights:list):
+    # Initial tests
+    if len(balance) != 2:
+        return None
+    if len(weights) < 1:
+        return None
+    
+    # Initial prints.
+    print('Input :', balance, '\t', weights)
+    solution = None
+
+    # Check if single value match.
+    x = single_match(balance, weights)
+
+    # If not single, maybe double?
+    if x is None and len(weights) > 1:
+        x = double_match(balance, weights)
+        x.sort()
+
+    # Final prints and return.
+    if x is None:
+        solution = 'Ikke mulig!'
+    else:
+        solution = x
+    print(solution)
+    return solution
---
+++ tests/test_1_1_4.py
@@ -0,0 +1,56 @@
+from app.set1.difficulty1.task4 import *
+
+
+def test_single_match():
+    balance = [3, 4]
+    weights = [1, 2, 7, 7]
+    assert single_match(balance, weights) == 1
+
+    weights.pop(0)
+    assert single_match(balance, weights) == None
+
+
+def test_double_balance():
+    balance = [13, 4]
+    assert not double_balance(balance, 2, 3)
+    assert double_balance(balance, 3, 6)
+
+
+def test_double_match():
+    balance = [13, 4]
+    weights = [1, 2, 3, 6, 14]
+    assert double_match(balance, weights) == [3, 6]
+
+    weights.pop(2)
+    assert double_match(balance, weights) == None
+
+    weights.append(5)
+    assert double_match(balance, weights) == [5, 14]
+
+    weights.pop(-1)
+    weights.append(-5)
+    assert double_match(balance, weights) == [-5, 14]
+
+
+def test_balancing():
+    # All empty.
+    assert balancing([], []) == None
+
+    # No weights.
+    assert balancing([1, 2], []) == None
+
+    # Wrong number of sides on the balance.
+    assert balancing([1, 2, 3], [4, 5, 6]) == None
+    assert balancing([1], [2, 3]) == None
+
+    # No solution.
+    assert balancing([1, 2], [3]) == 'Ikke mulig!'
+
+    # Single solution.
+    assert balancing([1, 3], [2]) == 2
+
+    # Double solution.
+    assert balancing([1, 2], [3, 4]) == [3, 4]
+
+    # Sorting
+    assert balancing([1, 2], [4, 3]) == [3, 4]
